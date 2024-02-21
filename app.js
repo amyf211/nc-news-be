a3fe505c35235e8c1498fa1a85bea5a3ebbbed98
Amy Fisher <amyfisher@Amys-MacBook-Pro.local>
2024-02-21 15:43:56 +0000

    10 add users endpoint and update endpoints.json

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const {getTopics, getEndpoints} = require('./controllers/topics-controller.js')
 const {getArticleById, getArticles} = require('./controllers/articles-controller.js')
 const {getCommentsByArticleId} = require('./controllers/comments-controller.js')
+const {getUsers} = require('./controllers/users-controller.js')
 const { handleCustomErrors, handleOtherErrors, handleDatabaseErrors } = require('./controllers/errors-controller.js')
 
 
@@ -15,6 +16,8 @@ app.get('/api/articles/:article_id', getArticleById)
 app.get('/api/articles', getArticles)
 
 app.get('/api/articles/:article_id/comments', getCommentsByArticleId)
+
+app.get('/api/users', getUsers)
 
 app.use(handleCustomErrors)
 
